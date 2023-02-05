c92fd631ff53808a2aba5162e9abe86dbf0ae789
Geofias <tamir-chong@hotmail.com>
2023-02-05 13:14:07 -0500

    Finished scraping the temperature and precipitation tables

--- clustering_tables.py
+++ clustering_tables.py
@@ -4,22 +4,23 @@ import numpy as np
 import pandas as pd
 
 url_precipitation = "https://en.wikipedia.org/wiki/List_of_countries_by_average_annual_precipitation"
+url_temperature = "https://en.wikipedia.org/wiki/List_of_countries_by_average_yearly_temperature"
 
-def Precipitation_table(url):
-    '''The function scrapes the countries average precipitation table from Wikipedia'''
+def Scraping_table(url):
+    '''The function scrapes the countries average precipitation and temperature tables from Wikipedia'''
     
     response = r.get(url)
 
-    precipitation_text = response.text
-    soup = BeautifulSoup(precipitation_text, "html.parser")
+    response_text = response.text
+    soup = BeautifulSoup(response_text, "html.parser")
 
-    precipitation_table = soup.table
+    scraped_table = soup.table
 
-    headers = precipitation_table.find_all("th")
+    headers = scraped_table.find_all("th")
     headers = [header.text.strip() for header in headers]
 
     rows = []
-    data_rows = precipitation_table.find_all("tr")
+    data_rows = scraped_table.find_all("tr")
 
     for row in data_rows:
         value = row.find_all("td")
@@ -32,6 +33,10 @@ def Precipitation_table(url):
     df = pd.DataFrame(rows, columns=headers)
     return df
 
-precipitation = Precipitation_table(url_precipitation)
-print(precipitation.info())
-print(precipitation.head())
+precipitation = Scraping_table(url_precipitation)
+
+temperature = Scraping_table(url_temperature)
+temperature.iloc[187:190:2, 0] = ["Norway", "Denmark"]
+
+precipitation.to_csv("./cluster_tables/precipitation.csv", index=False)
+temperature.to_csv("./cluster_tables/temperature.csv", index=False)
