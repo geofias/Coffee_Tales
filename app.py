a5456419f7649e53a30f17d2f3730ad14394835f
Geofias <89147046+geofias@users.noreply.github.com>
2024-07-14 00:20:46 -0500

    Adding code for streamlit deploying

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from PIL import Image
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+
+# Loading trained moel
+model = tf.keras.models.load_model('C:/Users/tamir/Documents/Projects/CoffeeLeaves_Classification/model/coffee_model', 
+                                   custom_objects={'KerasLayer':hub.KerasLayer})
+
+st.title('Coffe Leaf Disease Detection')
+
+class_names = ['[UNK]', 'healthy', 'rust', 'miner', 'cerscospora', 'phoma']
+
+uploaded_file = st.file_uploader("Load a coffee leaf image...", type="jpg")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Loaded Image', use_column_width=True)
+    
+    # Preprocese the image
+    img = image.resize((224, 224))
+    img = tf.keras.preprocessing.image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    
+    # Make the prediction
+    prediction = model.predict(img)
+    confidence = round(np.max(prediction[0]) * 100, 2)
+    predicted_class = class_names[np.argmax(prediction[0])]
+    st.write(f'Prediction: {predicted_class}')
+    st.write(f'Confidence: {confidence}')
